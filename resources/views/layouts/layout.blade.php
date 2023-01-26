1b90935eecc2bd61859ee1f20ab6a4e90278c721
jomar567 <cladojomar881@gmail.com>
2023-01-26 12:06:48 +0800

    install flowbite

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -15,5 +15,8 @@
     </main>
 
     @include('../components/footer')
+
+    {{-- FLOWBITE JS --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
 </body>
 </html>
