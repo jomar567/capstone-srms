300cd2ed70d6c739f1fc94932682c480e90e8d9d
jomar567 <cladojomar881@gmail.com>
2023-01-26 11:27:08 +0800

    installed tailwind

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.layout')
+
+@section('content')
+    <div class="hero">
+        Hero
+    </div>
+    <h1 class="bg-gray-800 text-white text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    <div>
+        Notice
+    </div>
+@endsection
