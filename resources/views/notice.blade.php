da5be8af03fa44b13eeec0e9c2101ce7214d10cb
jomar567 <cladojomar881@gmail.com>
2023-01-28 13:13:40 +0800

    notice page

--- resources/views/notice.blade.php
+++ resources/views/notice.blade.php
@@ -4,15 +4,8 @@
     <header class="flex flex-col items-center justify-center h-screen bg-hero bg-center bg-cover bg-no-repeat">
       <div class="absolute bg-slate-200/[.20] grid place-items-center h-screen w-screen top-0 left-0"></div>
         <div class="container relative mx-auto z-10 text-center flex flex-col items-center justify-center h-screen">
-          <p class="md:text-5xl text-xl font-medium md:mb-3 mb-1 tracking-normal md:tracking-wide">STUDENT</p>
-          <p class="md:text-7xl text-2xl font-bold tracking-wide md:tracking-wider md:mb-3 mb-1">RESULT</p>
-          <p class="md:text-5xl text-xl font-medium md:mb-3 mb-1 tracking-normal md:tracking-wide">MANAGEMENT</p>
-          <p class="md:text-5xl text-xl font-medium md:mb-3 mb-1 tracking-normal md:tracking-widest">SYSTEM</p>
-          {{-- <a href={{url('notice')}}> --}}
-            <button type="button" class="text-white bg-red hover:bg-blue focus:ring-4 focus:outline-none focus:ring-blue font-semibold rounded-lg md:text-base text-sm mt-6 md:px-10 px-5 md:py-3.5 py-2 text-center dark:bg-blue dark:hover:bg-blue dark:focus:ring-blue">
-              Learn More
-            </button>
-          {{-- </a> --}}
+          <p class="md:text-7xl text-2xl font-bold tracking-wide md:tracking-wider md:mb-3 mb-1">NOTICE</p>
+          <p class="md:text-5xl text-xl font-medium md:mb-3 mb-1 tracking-normal md:tracking-wide">BOARD</p>
           <span class="absolute left-0 flex flex-col ml-4">
             <a href="#">
               <i class="fa-brands fa-facebook md:text-2xl text-l hover:text-redpink cursor-pointer mb-8"></i>
@@ -29,15 +22,12 @@
         </div>
     </header>
 
-    <div class="grid container gap-4 mx-auto px-4 py-44 md:grid-cols-2">
+    {{-- <div class="grid container gap-4 mx-auto px-4 py-44 md:grid-cols-2">
         <div class="mt-8">
           <p class="text-5xl font-semibold before:border-l-8 before:border-redpink before:mr-4">Notice Board</p>
-          <p class="text-xl text-justify mt-10">
-            <a href={{url('notice')}}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-            </a>
-          </p>
+          <p class="text-xl text-justify mt-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+            </p>
           </div>
         <img class="hidden md:block" src="{{URL('images/notice.png')}}" alt="">
-    </div>
+    </div> --}}
 @endsection
