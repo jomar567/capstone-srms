02dddd6fa8ce15c5c9d6adc22b2f8d17171d387d
jomar567 <cladojomar881@gmail.com>
2023-01-28 14:45:27 +0800

    student login ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,8 @@ module.exports = {
         'wider': '30px'
       },
       backgroundImage: {
-        'hero': "url('https://media.istockphoto.com/id/1278496180/photo/exam-at-school-with-students-taking-educational-admission-test-in-class-thinking-hard-writing.jpg?s=612x612&w=0&k=20&c=JwMsJq9KYgu3zc1_5EdWSz_Y9n9i-qAziRpZIU5RauI=')"
+        'hero': "url('https://media.istockphoto.com/id/1278496180/photo/exam-at-school-with-students-taking-educational-admission-test-in-class-thinking-hard-writing.jpg?s=612x612&w=0&k=20&c=JwMsJq9KYgu3zc1_5EdWSz_Y9n9i-qAziRpZIU5RauI=')",
+        'login': "url('https://media.istockphoto.com/id/1216453954/photo/empty-classroom.jpg?s=612x612&w=0&k=20&c=HT2jBTNGKfxbFab3_c59hTqO_h36bc915PGYX5I1rNU=')"
       }
     },
   },
