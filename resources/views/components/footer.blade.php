40c3574c11062e2894422cb8e95f68c6e32a54b2
alandemol <alandemol05@gmail.com>
2023-01-30 20:12:39 +0800

    added footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,89 +1,75 @@
-<footer class="bg-white bg-blue-900  border-gray-200 m-5 p-5">
-    <div class="container flex flex-wrap">
-        <div class="flex flex-col md:flex-row ">
-            <div class="mx-3 pr-5 max-w-sm lg:max-w-none">
-                <h1 class="mt-6 flex justify-center gap-4 lg:justify-start"><strong class="text-white">LOGO</strong></h1>
-                <p class="text-xl text-justify mt-10 text-white ">SRMS is created using Laravel <br>and MySQL</p>
-                <a class="text-white grid gap-4 grid-cols-2 " href="#" target="_blank">
-                    <span class="sr-only"> Facebook </span>
 
-                    <svg class="h-6 w-6" fill="currentColor" viewBox="0 s0 24 24"aria-hidden="true">
-                        <path fill-rule="evenodd"
-                            d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                            clip-rule="evenodd" />
-                    </svg>
+<footer class="container mx-auto bg-blue m-5 p-5">
+    <div class="grid grid-cols-2">
+        <div class=" flex flex-col mb-6 px-10 md:mb-0 lg:my-8">
+            <a href="#" class="flex items-center">
+                <span class=" dark:text-white mb-8">LOGO</span>
+            </a>
+            <p class="mb-8">SRMS is created using Laravel<br>and MySQL</p>
+            <div class="flex flex-row ">
+                <a href="#">
+                    <svg class="w-5 h-5 hover:text-redpink" fill="currentColor"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
+                    <span class="sr-only">Facebook page</span>
                 </a>
-
-                <a class="text-white grid gap-4 grid-cols-2" href="#" target="_blank">
-                    <span class="sr-only"> Instagram </span>
-
-                    <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                        <path fill-rule="evenodd"
-                            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                            clip-rule="evenodd" />
-                    </svg>
+                <a href="#" class="px-5">
+                    <svg class="w-5 h-5 hover:text-redpink" fill="currentColor"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
+                    <span class="sr-only">Twitter page</span>
                 </a>
-
-                <a class="text-white grid gap-4 grid-cols-2" href="#" target="_blank">
-                    <span class="sr-only"> Twitter </span>
-
-                    <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                        <path
-                            d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
-                    </svg>
+                <a href="#">
+                    <svg class="w-5 h-5 hover:text-redpink" fill="currentColor"><path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" /></svg>
+                    <span class="sr-only">GitHub account</span>
                 </a>
-                <div class="grid grid-cols-1 gap-8 text-center lg:grid-cols-3 lg:text-left">
-                    <div>
-                        <strong class="font-medium text-white">Navigation</strong>
-                        <nav aria-label="Footer About Nav" class="mt-6 flex flex-col space-y-1">
-                            <a class="text-white transition hover:text-gray-700/75" href="/"><small>Home
-                                </small></a>
-                            <a class="text-white transition hover:text-gray-700/75" href="/"><small>Home
-                                </small></a>
-                            <a class="text-white transition hover:text-gray-700/75" href="/"><small>
-                                    Students</small></a>
-                            <a class="text-white transition hover:text-gray-700/75" href="/"><small>Admin
-                                </small></a>
-                        </nav>
-                    </div>
+            </div>
+        </div>
 
-                    <div>
-                        <strong class="font-medium text-white"> Contact us </strong>
-                        <nav aria-label="Footer Services Nav" class="mt-6 flex flex-col space-y-1">
-                            <a class="text-white transition hover:text-gray-700/75" href="#"><small>Makati,
-                                    Manila</small></a>
-                            <a class="text-white transition hover:text-gray-700/75" href="/"><small>(+63)
-                                    909981283</small></a>
-                            <a class="text-white transition hover:text-gray-700/75"
-                                href="/"><small>srms@result.com</small></a>
-                        </nav>
-                    </div>
-
-                    <div>
-                        <strong class="font-medium text-white">Subcribes</strong>
-                        <form class="mt-6">
-                            <div class="relative max-w-lg">
-                                <div class="block p-6 rounded-lg shadow-lg bg-white max-w-md">
-                                        <div class="form-group mb-6">
-                                            <input type="password"
-                                                class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                                                id="exampleInput126" placeholder="Get product updates">
-                                        </div>                                      
-                                </div>
-                                {{-- <input class="rounded border-gray-200 bg-gray-100  m-5 p-4 pr-15 text-sm font-small" placeholder="Get product updates" /> --}}
-                                <button
-                                    class="absolute rounded bg-blue-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-blue-700"
-                                    type="button">➜</button>
-                            </div>
-                        </form>
-                    </div>
+        <div class="grid grid-cols-2 gap-2 sm:gap-6 sm:grid-cols-3 pr-8">
+            <div>
+                <h2 class="mb-6 text-sm font-semibold text-gray-900 dark:text-white">Navigation</h2>
+                <ul class="text-gray-600 dark:text-gray-400">
+                    <li class="mb-4">
+                        <a href="#" class="hover:text-redpink"> <small>Home</small></a>
+                    </li>
+                    <li class="mb-4">
+                        <a href="#" class="hover:text-redpink"> <small>About</small></a>
+                    </li>
+                    <li class="mb-4">
+                        <a href="#" class="hover:text-redpink"> <small>Student</small></a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:text-redpink"> <small>Admin</small></a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h2 class="mb-6 text-sm font-semibold text-gray-900 dark:text-white">Contact us</h2>
+                <ul class="text-gray-600 dark:text-gray-400">
+                    <li class="mb-4">
+                        <a href="#" class="hover:text-redpink"><small>Makati</small></a>
+                    </li>
+                    <li class="mb-4">
+                        <a href="#" class="hover:text-redpink"><small>(+63) 909981283</small></a>
+                    </li class="mb-4">
+                    <li>
+                        <a href="#" class="hover:text-redpink"><small>srms@result.com</small></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="flex flex-col flex-row px-10 pr-0">
+                <h2 class="mb-6 text-sm font-semibold text-gray-900 dark:text-white">Subscribe</h2>
+                <div class="flex mt-3 space-x--4 sm:justify-center font-thin">
+                    <input type="text" class="form-control w-full px-1 text-bg-dark text-xs text-black rounded"  placeholder="Get product updates">
+                    <a href="" class="mr-1">
+                        <button type="submit" class="bg-redpink text-white font-bold py-2 px-2 rounded">➜</button>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
-    <hr class="mt-5">
-    <div class="container mt-5 flex flex-wrap text-white justify-between">
+    <hr class=" lg:my-8" />
+    <div class="sm:flex sm:items-center sm:justify-between mb-4 px-10">
         <p><small>Created by Group 6</small></p>
-        <p><small>Copyright © Student Result Management system 2023</small></p>
+        <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0 pr-1">
+            <p> <small>Copyright © Student Result Management System 2023</small></p>
+        </div>
     </div>
 </footer>
