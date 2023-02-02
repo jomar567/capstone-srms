2ec1ef1c0d84d29dae4a7e57a995deb673d16fb2
jomar567 <cladojomar881@gmail.com>
2023-02-02 16:34:06 +0800

    student result

--- resources/views/Admin/components/navbar.blade.php
+++ resources/views/Admin/components/navbar.blade.php
@@ -11,6 +11,13 @@
         <a href="https://flowbite.com" class="flex ml-2 md:mr-24">
           {{-- <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" /> --}}
           <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">SRMS</span>
+          {{-- <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
+            @if(Route::is('admin.dashboard'))
+              Dashboard
+            @elseif(Route::is('admin.dashboard'))
+            Student
+            @endif
+          </span> --}}
         </a>
       </div>
       <div class="flex items-center justify-center">
