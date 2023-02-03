d0ae02beb0a0e9622d3be63bfb70414d9dfdff3a
alandemol <alandemol05@gmail.com>
2023-02-03 10:45:38 +0800

    added valedation

--- resources/views/Admin/Courses/createCourse.blade.php
+++ resources/views/Admin/Courses/createCourse.blade.php
@@ -42,20 +42,35 @@
                   <label for="courseName" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">
                     Course Name
                   </label>
-                  <input type="text" id="courseName" name="courseName" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-              </div>
+                  <input type="text" id="courseName" name="courseName" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  @error('courseName')
+                  <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+                    <span class="font-medium">{{ $message }}</span>
+                  </p>
+                @enderror
+                </div>
               <div class="mb-6">
                   <label for="courseYearNumeric" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">
                     Course Year in Numeric
                   </label>
-                  <input type="number" id="courseYearNumeric" name="courseYearNumeric" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-              </div>
+                  <input type="number" id="courseYearNumeric" name="courseYearNumeric" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  @error('courseYearNumeric')
+                  <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+                    <span class="font-medium">{{ $message }}</span>
+                  </p>
+                @enderror
+                </div>
               <div class="mb-6">
                   <label for="section" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">
                     Section
                   </label>
-                  <input type="text" id="section" name="section" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-              </div>
+                  <input type="text" id="section" name="section" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  @error('section')
+                  <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+                    <span class="font-medium">{{ $message }}</span>
+                  </p>
+                @enderror
+                </div>
               <button type="submit" class="block mx-auto text-white bg-redpink hover:bg-blue focus:ring-4 focus:outline-none font-medium rounded-lg text-base px-6 py-2.5 text-center  mt-7">
                 Add Course
               </button>
