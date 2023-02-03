cba3828925880c748863491aba680af8bb4155e3
ZannBuce <zannbuce@gmail.com>
2023-02-03 10:42:27 +0800

    added edit error and success

--- resources/views/Admin/Subjects/editSubject.blade.php
+++ resources/views/Admin/Subjects/editSubject.blade.php
@@ -42,13 +42,23 @@
                 <label for="subjectName" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">
                   Subject Name
                 </label>
-                <input type="text" id="subjectName" name="subjectName" value="{{$subject->subjectName}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                <input type="text" id="subjectName" name="subjectName" value="{{$subject->subjectName}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                @error('subjectName')
+                  <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+                    <span class="font-medium">{{ $message }}</span>
+                  </p>
+                @enderror
               </div>
               <div class="mb-6">
                   <label for="subjectCode" class="block mb-2 text-base font-medium text-gray-900 dark:text-white">
                     Subject Code
                   </label>
-                  <input type="number" id="subjectCode" name="subjectCode" value="{{$subject->subjectCode}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                  <input type="number" id="subjectCode" name="subjectCode" value="{{$subject->subjectCode}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" >
+                  @error('subjectCode')
+                  <p class="mt-2 text-sm text-red-600 dark:text-red-500">
+                    <span class="font-medium">{{ $message }}</span>
+                  </p>
+                @enderror
               </div>
               <button type="submit" class="block mx-auto text-white bg-redpink hover:bg-blue focus:ring-4 focus:outline-none font-medium rounded-lg text-base px-6 py-2.5 text-center  mt-7">
                 Update Subject
