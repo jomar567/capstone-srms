3be7ec7d89a1ed75c05f794ceccaae70ce7bafb0
alandemol <alandemol05@gmail.com>
2023-02-09 14:23:21 +0800

    added successfully updated

--- app/Http/Controllers/Admin/NoticeController.php
+++ app/Http/Controllers/Admin/NoticeController.php
@@ -40,7 +40,7 @@ class NoticeController extends Controller
         $notices->description = $request->description;
         $notices->save();
 
-        return redirect()->route('admin.notice_list');
+        return redirect()->route('admin.notice_list')->with('success', 'Notice successfully updated!');
      }
 
      public function destroynotice($id){
