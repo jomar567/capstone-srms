931742f45088ca88a12ba5feb2164ba31836ca62
jomar567 <cladojomar881@gmail.com>
2023-02-10 19:20:52 +0800

    create a reusable function for getting result

--- app/Http/Controllers/Student/StudentController.php
+++ app/Http/Controllers/Student/StudentController.php
@@ -11,52 +11,55 @@ use App\Models\Result;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Auth;
 
+//Created a separate class for getting the student result
+class getResult {
+
+  public static function resultDetails() {
+    $students = Student::find(auth()->user()->id);
+    $courses = Course::where('id', $students->course_id)->get();
+    $results = Result::where('student_id', $students->id)->get();
+    $totalScore = Result::where('student_id', $students->id)->count()*100;
+    $sum = Result::where('student_id', $students->id)->sum('grades');
+    if($totalScore && $sum > 0) {
+      $average = ($sum / $totalScore) * 100;
+    } else {
+      $average = 0;
+    }
+    $formatted_average = number_format($average, 1);
+
+    $data = [
+      'students' => $students,
+      'courses' => $courses,
+      'results' => $results,
+      'totalScore' => $totalScore,
+      'sum' => $sum,
+      'formatted_average' => $formatted_average,
+    ];
+
+    return $data;
+  }
+
+}
+
 class StudentController extends Controller
 {
-    public function createPDF(){
-      $students = Student::find(auth()->user()->id);
-      $courses = Course::where('id', $students->course_id)->get();
-      $results = Result::where('student_id', $students->id)->get();
-      $totalScore = Result::where('student_id', $students->id)->count()*100;
-      $sum = Result::where('student_id', $students->id)->sum('grades');
-      if($totalScore && $sum > 0) {
-        $average = ($sum / $totalScore) * 100;
-      } else {
-        $average = 0;
-      }
-      $formatted_average = number_format($average, 1);
+    public function createPDF(Request $request){
+      $data = getResult::resultDetails();
 
-      $data = [
-          'students' => $students,
-          'courses' => $courses,
-          'results' => $results,
-          'totalScore' => $totalScore,
-          'sum' => $sum,
-          'formatted_average' => $formatted_average,
-      ];
       $pdf = PDF::loadView('Student.components.resultPDF', $data);
 
       return $pdf->stream('result.pdf');
     }
 
-    public function resultDetails(Request  $request) {
-      $students = Student::find(auth()->user()->id);
-      $courses = Course::where('id', $students->course_id)->get();
-      $results = Result::where('student_id', $students->id)->get();
-      $totalScore = Result::where('student_id', $students->id)->count()*100;
-      $sum = Result::where('student_id', $students->id)->sum('grades');
-      if($totalScore && $sum > 0) {
-        $average = ($sum / $totalScore) * 100;
-      } else {
-        $average = 0;
-      }
-      $formatted_average = number_format($average, 1);
+    public function dashboardResult(Request $request) {
+      $data = getResult::resultDetails();
 
-      return view('Student.Result')->with('students', $students)
-        ->with('courses', $courses)
-        ->with('totalScore', $totalScore)
-        ->with('sum', $sum)
-        ->with('formatted_average', $formatted_average)
-        ->with('results', $results);
+      return view('Student.Dashboard', $data);
+    }
+
+    public function studentResult(Request $request) {
+      $data = getResult::resultDetails();
+
+      return view('Student.Result', $data);
     }
 }
