37a6a97398671102cbe520eb25970387e06726f9
ZannBuce <zannbuce@gmail.com>
2023-02-11 17:28:23 +0800

    Fixed Logo

--- resources/views/Student/components/navbar.blade.php
+++ resources/views/Student/components/navbar.blade.php
@@ -9,10 +9,9 @@
                <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
             </svg>
          </button>
-        <a href="https://flowbite.com" class="flex ml-2 md:mr-24">
-          
+        <a class="flex ml-2 md:mr-24">
           <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-          <img src="{{URL('images/GroupB.png')}}" alt="" class="h-8 mr-3"/> 
+            <img src="{{URL('images/GroupB.png')}}" alt="" class="h-8 mr-3"/> 
           </span>
         </a>
       </div>
