dfb315a8aed8c5a7bba4e2bf2a5a3d72222ccab0
jomar567 <cladojomar881@gmail.com>
2023-02-14 17:23:10 +0800

    pdf favicon

--- resources/views/Admin/layouts/layout.blade.php
+++ resources/views/Admin/layouts/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>SRMS</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
     {{-- Toaster CSS --}}
@@ -26,43 +26,6 @@
       </main>
 
     @include('../Admin/components/sidebar')
-
-    {{-- <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header d-flex justify-content-center">
-                        <h1>Welcome to Admin Dashboard</h1>
-                        <ul>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::guard('admin')->user()->username }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('admin.logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('admin.logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-
-                    <div class="card-body">
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
-
-
 
 </body>
 </html>
--- resources/views/Student/components/resultPDF.blade.php
+++ resources/views/Student/components/resultPDF.blade.php
@@ -17,109 +17,111 @@
       width: 100%;
       height: 100%;
     }
-    .studentDetails {
-      display: inline-block;
-      vertical-align: middle
-      color: red;
+    .column {
+      float: left;
+      width: 50%;
+      padding: 10px;
+    }
+    .row:after {
+      content: "";
+      display: table;
+      clear: both;
+    }
+    .fs-3 {
+      font-size: 16px;
+      font-weight: 400;
     }
   </style>
 </head>
 <body>
 
-  <div class="studentDetails">
-    <p>asdad</p>
-    <p>asdad</p>
+  <div class="row">
+    @foreach($courses as $course)
+      <div class="column">
+        <strong><p>Name: <span class="fs-3">{{$students->fullName}}</span></p></strong>
+        <strong><p>Student ID: <span class="fs-3">{{$students->student_ID}}</span></p></strong>
+
+      </div>
+      <div class="column">
+        <strong><p>Course: <span class="fs-3">{{$course->courseName}}</span></p></strong>
+        <strong><p>Year & Section: <span class="fs-3">{{$course->courseYearNumeric}}{{$course->section}}</span></p></strong>
+      </div>
+    @endforeach
   </div>
 
-    <div class="wrapper text-redpink">
-    @foreach($courses as $course)
-    <div class="row1">
-    Name: <span>{{$students->fullName}}</span>
-    Course: <span>{{$course->courseName}}</span>
-    </div>
-
-      Student ID: <span>{{$students->student_ID}}</span>
-      Year & Section: <span>{{$course->courseYearNumeric}}{{$course->section}}</span>
-
-    @endforeach
-    </div>
-
-
-
-        {{-- Table --}}
-
-        <div>
-          <table class="table table-bordered">
-              <thead>
-                  <tr>
-                      <th scope="col">
-                          #
-                      </th>
-                      <th scope="col">
-                          Subject
-                      </th>
-                      <th scope="col">
-                          Marks
-                      </th>
-                  </tr>
-              </thead>
-              <tbody>
-              @if(count($results) > 0)
-                  @foreach($results as $result)
-                    <tr class="bg-light border-b dark:bg-gray-800 dark:border-gray-700">
-                      <td class="px-6 py-4">
-                        {{$result->id}}
-                      </th>
-                      <td class="px-6 py-4">
-                        {{$result->subject->subjectName}}
-                      </td>
-                      <td class="px-6 py-4">
-                        {{$result->grades}}
-                      </td>
-                  </tr>
-                  @endforeach
-                  @else
-                  <tr>
-                    <td colspan="3" class="text-center px-6 py-4">
-                      No results found
-                    </td>
-                  </tr>
+  {{-- Table --}}
+  <div>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th scope="col">
+                    #
+                </th>
+                <th scope="col">
+                    Subject
+                </th>
+                <th scope="col">
+                    Marks
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+        @if(count($results) > 0)
+            @foreach($results as $result)
+              <tr class="bg-light border-b dark:bg-gray-800 dark:border-gray-700">
+                <td class="px-6 py-4">
+                  {{$result->id}}
+                </th>
+                <td class="px-6 py-4">
+                  {{$result->subject->subjectName}}
+                </td>
+                <td class="px-6 py-4">
+                  {{$result->grades}}
+                </td>
+            </tr>
+            @endforeach
+            @else
+            <tr>
+              <td colspan="3">
+                No results found
+              </td>
+            </tr>
+          @endif
+          <tr class="bg-light border dark:bg-gray-800">
+            <td colspan="2" class="text-base font-semibold border text-center px-6 py-4">
+              <strong>Total Score</strong>
+            </td>
+            <td class="px-6 py-4 text-center">
+                <span class="text-base font-semibold">{{$sum}}</span>
+                out of
+                <span class="text-base font-semibold">{{$totalScore}}</span>
+            </td>
+          </tr>
+          <tr class="bg-light border dark:bg-gray-800">
+            <td colspan="2" class="text-base font-semibold border text-center px-6 py-4">
+              <strong>Percentage</strong>
+            </td>
+            <td class="px-6 py-4 text-center">
+                <p class="text-base font-semibold">{{$formatted_average}}%</p>
+            </td>
+          </tr>
+          <tr class="bg-light border dark:bg-gray-800">
+            <td colspan="2" class="text-base font-semibold border text-center px-6 py-4">
+              <strong>Remarks</strong>
+            </td>
+            <td class="px-6 py-4 text-center">
+              <p class="text-base font-semibold">
+                @if($formatted_average > 0)
+                  {{$formatted_average >= 75 ? 'PASSED' : 'FAILED'}}
+                @else
+                  N/A
                 @endif
-                <tr class="bg-light border dark:bg-gray-800">
-                  <td colspan="2" class="text-base font-semibold border text-center px-6 py-4">
-                    Total Score
-                  </td>
-                  <td class="px-6 py-4 text-center">
-                      <span class="text-base font-semibold">{{$sum}}</span>
-                      out of
-                      <span class="text-base font-semibold">{{$totalScore}}</span>
-                  </td>
-                </tr>
-                <tr class="bg-light border dark:bg-gray-800">
-                  <td colspan="2" class="text-base font-semibold border text-center px-6 py-4">
-                    Percentage
-                  </td>
-                  <td class="px-6 py-4 text-center">
-                      <p class="text-base font-semibold">{{$formatted_average}}%</p>
-                  </td>
-                </tr>
-                <tr class="bg-light border dark:bg-gray-800">
-                  <td colspan="2" class="text-base font-semibold border text-center px-6 py-4">
-                    Remarks
-                  </td>
-                  <td class="px-6 py-4 text-center">
-                    <p class="text-base font-semibold">
-                      @if($formatted_average > 0)
-                        {{$formatted_average >= 75 ? 'PASSED' : 'FAILED'}}
-                      @else
-                        N/A
-                      @endif
-                    </p>
-                  </td>
-                </tr>
-              </tbody>
-          </table>
-        </div>
+              </p>
+            </td>
+          </tr>
+        </tbody>
+    </table>
+  </div>
 
 </body>
 </html>
